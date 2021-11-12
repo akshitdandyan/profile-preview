cb4aaa52a8477535fb29782309edf930f772e247
mahi-14 <mahi1206@chitkara.edu.in>
2021-11-12 23:19:21 +0530

    get code

---
+++ src/Components/profileInfo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export class profileInfo extends React.Component{
+  
+  render(){
+    return(
+      <h1>hello world</h1>
+    )
+  }
+}
+export default profileInfo;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//mport profileInfo from './profileInfo';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
